66a852e8ade2b29770fb740c8ecdcb0ad5aa1320
dev-santi <bauzalarrosasantiago@gmail.com>
2024-02-21 02:40:22 -0300

    funcionalidad del selector destino

--- js/crear-reunion.js
+++ js/crear-reunion.js
@@ -1,28 +1,29 @@
-window.addEventListener("load", programa);
-
 const ciudades = [
     {
-        nombre: "Bogotá",
-        codigo: "America/Bogota",
+        nombre: 'Bogotá',
+        codigo: 'America/Bogota',
     },
     {
-        nombre: "Buenos Aires",
-        codigo: "America/Buenos_Aires",
+        nombre: 'Buenos Aires',
+        codigo: 'America/Buenos_Aires',
     },
     {
-        nombre: "Montevideo",
-        codigo: "America/Montevideo",
+        nombre: 'Montevideo',
+        codigo: 'America/Montevidseo',
     },
 ];
+const ciudadesAIncluir = [];
 
+window.addEventListener('load', programa);
 function programa() {
-    /* Agregamos las ciudades disponibles a los selectores */
-    const selectorDeOrigen = document.getElementById("idSelectOriginCity");
-    const selectorDeDestino = document.getElementById("idSelectDestCity");
+    const selectorDeOrigen = document.getElementById('idSelectOriginCity');
+    const selectorDeDestino = document.getElementById('idSelectDestCity');
+    const listaDePaisesIncluidos = document.getElementById('idSelectedCities');
 
+    // Agregamos las ciudades disponibles a los selectores
     ciudades.forEach((ciudad) => {
-        const opcionOrigen = document.createElement("option");
-        const opcionDestino = document.createElement("option");
+        const opcionOrigen = document.createElement('option');
+        const opcionDestino = document.createElement('option');
 
         opcionOrigen.value = ciudad.codigo;
         opcionOrigen.textContent = ciudad.nombre;
@@ -32,4 +33,45 @@ function programa() {
         selectorDeOrigen.appendChild(opcionOrigen);
         selectorDeDestino.appendChild(opcionDestino);
     });
+
+    // Cada vez que se elija una opcion de ciudad destino,
+    // se agrega a la lista de ciudades a incluir y se actualiza la interfaz
+    selectorDeDestino.addEventListener('change', () => {
+        if (selectorDeDestino.value) {
+            const ciudadNueva = ciudades.find(
+                (ciudad) => ciudad.codigo === selectorDeDestino.value
+            );
+            ciudadesAIncluir.push(ciudadNueva);
+            actualizarInterfazDeCiudadesIncluidas();
+        }
+    });
+
+    //Funciones
+    function actualizarInterfazDeCiudadesIncluidas() {
+        //Se borra la lista y se renderiza de nuevo.
+        listaDePaisesIncluidos.innerHTML = '';
+        ciudadesAIncluir.forEach((ciudad) => {
+            const ciudadAgregada = document.createElement('li');
+            const cruzRoja = document.createElement('img');
+
+            ciudadAgregada.textContent = ciudad.nombre;
+            cruzRoja.src = '../assets/img/cross.png';
+            cruzRoja.alt = 'Icono con forma de una cruz roja.';
+            cruzRoja.addEventListener('click', () => eliminarOpcion(ciudad));
+
+            ciudadAgregada.appendChild(cruzRoja);
+            listaDePaisesIncluidos.appendChild(ciudadAgregada);
+        });
+    }
+
+    function eliminarOpcion(ciudad) {
+        let continuarCiclo = true;
+        for (let i = 0; i < ciudadesAIncluir.length && continuarCiclo; i++) {
+            if (ciudadesAIncluir[i].codigo === ciudad.codigo) {
+                ciudadesAIncluir.splice(i, 1);
+                continuarCiclo = false;
+            }
+        }
+        actualizarInterfazDeCiudadesIncluidas();
+    }
 }
