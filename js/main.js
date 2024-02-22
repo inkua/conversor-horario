005fcebb6409e5af1836ec4c0c9f3b32dbdd6238
julianalvarez243 <julianalvarez0203@gmail.com>
2024-02-22 20:23:40 -0300

    conversor de hora actualizado.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,46 @@
 function mostrarHoraConvertida() {
-    const hora = document.getElementById('hora{local').value;
-    const paisOrigen = document.getElementById('paisOrigen').value;
-    const paisDestino = document.getElementById('paisDestino').value;
-    const titulo = document.getElementById('title').value;
-    const descripcion = document.getElementById('description').value;
-    const link = document.getElementById('linkMeet').value;
-    const horaEnDestino = convertirHora(hora, paisOrigen, paisDestino);
-
+    const hora = (document.getElementById('horaLocal').value);
+    const ciudadOrigen = document.getElementById('ciudadOrigen').value;
+    const ciudadDestino = document.getElementById('ciudadDestino').value;
+    const horaEnDestino = convertirHora(hora, ciudadOrigen, ciudadDestino);
+  
     document.getElementById('resultado').style.display = 'block';
-    console.log(hora);
-    document.getElementById(
-        'tituloResultado'
-    ).innerHTML = `Titulo de la reunion: ${titulo}`;
-    document.getElementById('descripcionResultado').innerHTML = descripcion;
-    document.getElementById('linkResultado').innerHTML = link;
-    document.getElementById(
-        'horaResultado'
-    ).innerHTML = `La hora en ${paisDestino} es: ${horaEnDestino}`;
-}
+    document.getElementById('resultado').innerHTML = `La hora en ${ciudadDestino} es: ${horaEnDestino}`;
 
-function convertirHora(hora, paisOrigen, paisDestino) {
-    const horaMoment = moment(hora, 'HH:mm', true);
+  }
+
+function convertirHora(hora, ciudadOrigen, ciudadDestino) {
+    const horaMoment = moment(hora, 'HH:mm', true); 
     //true valida que la hora sea correcta, lo recomienda la documentacion
     /*                 moment(String, String, boolean);
     genera un objeto moment llamado horaMoment para posteriormente trabajar con los metodos hours y minutes sobre el*/
-
-    const fechaOrigen = moment.tz(moment(), obtenerZonaHoraria(paisOrigen));
-
+  
+    const fechaOrigen = moment.tz(moment(), obtenerZonaHoraria(ciudadOrigen));
+  
     fechaOrigen.hours(horaMoment.hours());
     fechaOrigen.minutes(horaMoment.minutes());
-
-    const horaDestino = fechaOrigen
-        .clone()
-        .tz(obtenerZonaHoraria(paisDestino))
-        .format('hh:mm A');
-
+  
+    const horaDestino = fechaOrigen.clone().tz(obtenerZonaHoraria(ciudadDestino)).format('hh:mm A');
+  
     return horaDestino;
-}
+    }
 
-function obtenerZonaHoraria(pais) {
+  function obtenerZonaHoraria(ciudad) {
     const zonasHorarias = {
-        Argentina: 'America/Argentina/Buenos_Aires',
-        Bolivia: 'America/La_Paz',
-        Brazil: 'America/Sao_Paulo',
-        Chile: 'America/Santiago',
-        Colombia: 'America/Bogota',
-        Ecuador: 'America/Guayaquil',
-        Paraguay: 'America/Asuncion',
-        Peru: 'America/Lima',
-        Uruguay: 'America/Montevideo',
-        Venezuela: 'America/Caracas',
-        // Se agregan paises segun se necesiten
+      'Buenos Aires': 'America/Argentina/Buenos_Aires',
+      'La paz': 'America/La_Paz',
+      'Sao Paulo': 'America/Sao_Paulo',
+      'Chile': 'America/Santiago',
+      'Bogota': 'America/Bogota',
+      'Ecuador': 'America/Guayaquil',
+      'Paraguay': 'America/Asuncion',
+      'Peru': 'America/Lima',
+      'Uruguay': 'America/Montevideo',
+      'Venezuela': 'America/Caracas',
+      // Se agregan ciudades segun se necesiten 
     };
 
-    return zonasHorarias[pais] || 'UTC';
-}
+    return zonasHorarias[ciudad] || 'UTC';
+  }
 
-// Documentacion de moment-timezone: https://momentjs.com/timezone/docs/
+  // Documentacion de moment-timezone: https://momentjs.com/timezone/docs/ 
