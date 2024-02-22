c3f0dc2ca79e11f82c88b9a98e05c2f9847130e8
dev-santi <bauzalarrosasantiago@gmail.com>
2024-02-21 21:25:42 -0300

    pagina crear reunion v1.0 terminada

--- js/crear-reunion.js
+++ js/crear-reunion.js
@@ -8,11 +8,79 @@ const ciudades = [
         codigo: 'America/Buenos_Aires',
     },
     {
+        nombre: 'Cordoba',
+        codigo: 'America/Cordoba',
+    },
+    {
         nombre: 'Montevideo',
-        codigo: 'America/Montevidseo',
+        codigo: 'America/Montevideo',
+    },
+
+    {
+        nombre: 'Santiago de Chile',
+        codigo: 'America/Santiago',
+    },
+    {
+        nombre: 'Asunción',
+        codigo: 'America/Asuncion',
+    },
+    {
+        nombre: 'La Paz',
+        codigo: 'America/La_Paz',
+    },
+    {
+        nombre: 'Lima',
+        codigo: 'America/Lima',
+    },
+    {
+        nombre: 'Guayaquil',
+        codigo: 'America/Guayaquil',
+    },
+    {
+        nombre: 'Panama',
+        codigo: 'America/Panama',
+    },
+    {
+        nombre: 'El Salvador',
+        codigo: 'America/El_Salvador',
+    },
+    {
+        nombre: 'Puerto Rico',
+        codigo: 'America/Puerto_Rico',
+    },
+    {
+        nombre: 'Ciudad de Mexico',
+        codigo: 'America/Mexico_City',
     },
 ];
 const ciudadesAIncluir = [];
+//Este objeto almacena las conversiones para el formato de las fechas,
+//por defecto toString() devuelve valores en ingles.
+const traductor = {
+    dias: {
+        Mon: 'Lunes',
+        Tue: 'Martes',
+        Wed: 'Miércoles',
+        Thu: 'Jueves',
+        Fri: 'Viernes',
+        Sat: 'Sábado',
+        Sun: 'Domingo',
+    },
+    meses: {
+        Jan: '01',
+        Feb: '02',
+        Mar: '03',
+        Apr: '04',
+        May: '05',
+        Jun: '06',
+        Jul: '07',
+        Aug: '08',
+        Sep: '09',
+        Oct: '10',
+        Nov: '11',
+        Dec: '12',
+    },
+};
 
 window.addEventListener('load', programa);
 function programa() {
@@ -53,7 +121,97 @@ function programa() {
         }
     });
 
+    //Generar resultado
+    const formulario = document.getElementById('idInputs');
+    const resultadoContenedor = document.getElementById('idOutputContent');
+    const boton = document.getElementById('idBtnSubmit');
+    boton.addEventListener('click', generarResultado);
+
     //Funciones
+
+    function generarResultado() {
+        if (formulario.reportValidity()) {
+            // Reseteo del contenedor con la respuesta.
+            resultadoContenedor.innerHTML = '';
+
+            const titulo = document.getElementById('idTitle').value;
+            const descripcion = document.getElementById('idDescription').value;
+            const link = document.getElementById('idLink').value;
+            const fecha = document.getElementById('idDate').value;
+            const ciudadDeOrigen = document.getElementById('idSelectOriginCity').value;
+
+            const tituloContenedor = document.createElement('h2');
+            let descripcionContenedor = 'Sin descripción';
+            if (descripcion) {
+                descripcionContenedor = document.createElement('p');
+            }
+            const linkContenedor = document.createElement('a');
+            const fechasContenedor = document.createElement('ul');
+            const contenedorBotonCopiar = document.createElement('button');
+            const botonDeCopiar = document.createElement('img');
+
+            tituloContenedor.textContent = titulo;
+            descripcionContenedor.textContent = descripcion;
+            linkContenedor.textContent = 'Link a la reunión: ' + link;
+            linkContenedor.href = link;
+
+            botonDeCopiar.src = '../assets/img/copy.png';
+            botonDeCopiar.alt = 'Icono de una hoja encima de otra.';
+            contenedorBotonCopiar.appendChild(botonDeCopiar);
+            contenedorBotonCopiar.type = 'button';
+            contenedorBotonCopiar.id = 'idCopy';
+
+            //Creamos la fecha ingresada en su ciudad de origen utilizando momentjs,
+            //Luego, iteramos las ciudades a incluir y hacemos las conversiones
+            const fechaInicial = moment.tz(fecha, ciudadDeOrigen);
+            let acumuladorDeFechasParaCopiar = '';
+            ciudadesAIncluir.forEach((ciudad) => {
+                const fechaContenedor = document.createElement('li');
+                const fechaFinal = fechaInicial.clone().tz(ciudad.codigo);
+                const [dia, mes, numeroDia, anio, hora] = fechaFinal.toString().split(' ');
+                const resultado = `${ciudad.nombre}: ${traductor.dias[dia]} ${numeroDia}/${
+                    traductor.meses[mes]
+                } - ${hora.slice(0, 5)}`;
+
+                acumuladorDeFechasParaCopiar += resultado + '\n';
+
+                fechaContenedor.textContent = resultado;
+                fechasContenedor.appendChild(fechaContenedor);
+            });
+
+            resultadoContenedor.appendChild(tituloContenedor);
+            resultadoContenedor.appendChild(descripcionContenedor);
+            resultadoContenedor.appendChild(linkContenedor);
+            resultadoContenedor.appendChild(fechasContenedor);
+
+            //Agregamos el evento click de poder copiar al portapapeles
+            contenedorBotonCopiar.addEventListener('click', () => {
+                const textoACopiar =
+                    titulo +
+                    '\n' +
+                    descripcion +
+                    '\n\n' +
+                    'Link a la reunión: ' +
+                    link +
+                    '\n\n' +
+                    acumuladorDeFechasParaCopiar;
+                console.log(textoACopiar);
+
+                // Generar un elemento para poder realizar la copia del texto y eliminarlo.
+                const elementoTemporal = document.createElement('textarea');
+                elementoTemporal.value = textoACopiar;
+                document.body.appendChild(elementoTemporal);
+                elementoTemporal.select();
+                //execCommand esta deprecado por evidentes problemas de
+                //seguridad, lo cual no nos preocupa porque no tenemos informacion sensible.
+                document.execCommand('copy');
+                document.body.removeChild(elementoTemporal);
+                alert('Texto copiado exitosamente');
+            });
+            resultadoContenedor.appendChild(contenedorBotonCopiar);
+        }
+    }
+
     function actualizarInterfazDeCiudadesIncluidas() {
         //Se borra la lista y se renderiza de nuevo.
         listaDePaisesIncluidos.innerHTML = '';
