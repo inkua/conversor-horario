eaf005c186e4b4df8d664a325dbfbca06aac1ab4
Manuel Florez <89274119+manuel14mds@users.noreply.github.com>
2024-02-24 23:17:09 -0500

    First review (#17)
    * create develop branch
    
    * not found upload
    
    * Base del proyecto
    
    * not found upload with style
    
    * not found upload with style
    
    * a
    
    * first commit
    
    * commit
    
    * Proyecto base
    
    * Proyecto base
    
    * re-organize scaffolding
    
    * Update readme
    
    Primera version del readme
    
    * 404 cargado con diseño, y sincronizado con el proyecto
    
    * 404 cargado con diseño, y sincronizado con el proyecto
    
    * HTML del creador de reunion
    
    * add heading nav
    
    * nuevo diseño converor
    
    * create page crear reunion
    
    * Trabajando en la pestaña Crear Reunión
    
    * funcionalidad del selector destino
    
    * nuevo diseño conversor
    
    * seccion de resultados
    
    * pagina crear reunion v1.0 terminada
    
    * Fixeo de link en la navegacion de crear reunion
    
    * conversor de hora actualizado.
    
    * fix navbar
    
    * Agregar página 404 de Faustino
    
    * not found page full responsive
    
    * add new style to conversor page
    
    ---------
    
    Co-authored-by: julianalvarez243 <julianalvarez0203@gmail.com>
    Co-authored-by: dev-santi <bauzalarrosasantiago@gmail.com>
    Co-authored-by: Johel Goñi <106790228+jhgoni@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,49 @@
+function mostrarHoraConvertida() {
+    const hora = (document.getElementById('horaLocal').value);
+    const ciudadOrigen = document.getElementById('ciudadOrigen').value;
+    const ciudadDestino = document.getElementById('ciudadDestino').value;
+    const horaEnDestino = convertirHora(hora, ciudadOrigen, ciudadDestino);
+  
+    document.getElementById('resultado').style.display = 'block';
+    document.getElementById('resultado').innerHTML = `La hora en ${ciudadDestino} es: ${horaEnDestino}`;
+
+  }
+
+function convertirHora(hora, ciudadOrigen, ciudadDestino) {
+    const horaMoment = moment(hora, 'HH:mm', true); 
+    //true valida que la hora sea correcta, lo recomienda la documentacion
+    /*                 moment(String, String, boolean);
+    genera un objeto moment llamado horaMoment para posteriormente trabajar con los metodos hours y minutes sobre el*/
+  
+    const fechaOrigen = moment.tz(moment(), obtenerZonaHoraria(ciudadOrigen));
+  
+    fechaOrigen.hours(horaMoment.hours());
+    fechaOrigen.minutes(horaMoment.minutes());
+  
+    const horaDestino = fechaOrigen.clone().tz(obtenerZonaHoraria(ciudadDestino)).format('hh:mm A');
+  
+    return horaDestino;
+    }
+
+  function obtenerZonaHoraria(ciudad) {
+    const zonasHorarias = {
+      '🇨🇴 Bogota': 'America/Bogota',
+      '🇦🇷 Buenos Aires': 'America/Buenos_Aires',
+      '🇦🇷 Cordoba': 'America/Cordoba',
+      '🇺🇾 Montevideo': 'America/Montevideo',
+      '🇨🇱 Santiago de Chile': 'America/Santiago',
+      '🇵🇾 Asuncion': 'America/Asuncion',
+      '🇧🇴 La Paz': 'America/La_Paz',
+      '🇵🇪 Lima': 'America/Lima',
+      '🇪🇨 Guayaquil': 'America/Guayaquil',
+      '🇵🇦 Panama': 'America/Panama',
+      '🇸🇻 El Salvador': 'America/El_Salvador',
+      '🇵🇷 Puerto Rico': 'America/Puerto_Rico',
+      '🇲🇽 Ciudad de Mexico': 'America/Mexico_City'    
+      // Se agregan ciudades segun se necesiten 
+    };
+
+    return zonasHorarias[ciudad] || 'UTC';
+  }
+
+  // Documentacion de moment-timezone: https://momentjs.com/timezone/docs/ 
